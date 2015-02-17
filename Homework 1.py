50244aab2dabb23a1816051a09a0354ff77586ca
wardj96 <wardj96@bu.edu>
2015-02-17 12:00:15 -0500

    Test
    Test

---
+++ Homework 1.py
@@ -0,0 +1,79 @@
+
+# coding: utf-8
+
+# In[1]:
+
+person1 = input("Enter your name Person 1:")
+print('Hello', person1)
+person2 = input("Enter your name Person 2:")
+print('Hello', person2)
+
+
+# In[2]:
+
+number = int(input("Please eneter a number:"))
+sum = (((number + 3)*2 - 4) - (number * 2) + 3)
+print(sum)
+
+
+# In[3]:
+
+Temperature_F = int(input("please enter temperature in F degrees:"))
+Temperature_C = ((Temperature_F - 32) * (5/9))
+print ("The temperature is ", Temperature_C , "degrees Celcius")
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
